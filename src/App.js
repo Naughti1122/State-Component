3871a2c24bfff3820d89b91b4da35a064eda472f
Andy <andysworld40@gmail.com>
2022-03-24 15:27:20 -0700

    components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ClassState from './State Components/ClassState';
+import FunctionState from './State Components/FunctionState';
+
+
+function App() {
+  return (
+    <>
+    <ClassState />
+    <FunctionState />
+    </>
+  );
+}
+
+export default App;
